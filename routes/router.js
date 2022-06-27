9c905bc68f001a57633285319e652b5fd8ec4a8a
Kenneth <kennethlokoy@gmail.com>
2022-06-27 13:43:35 +0200

    removed cors

--- routes/router.js
+++ routes/router.js
@@ -1,11 +1,6 @@
 import express from 'express'
-import cors from 'cors'
 
 const router = express()
-
-router.use(cors({
-  origin: ['https://vest-cctv.herokuapp.com']
-}))
 
 router.use(express.static('client/build'))
 
