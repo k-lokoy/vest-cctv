c42bbf1a00d5b7640799051c1cd7cb7bb31288b8
Kenneth <kennethlokoy@gmail.com>
2022-06-28 08:15:28 +0200

    Removed debug logs

--- routes/bingreports.js
+++ routes/bingreports.js
@@ -3,12 +3,9 @@ import fetch from 'node-fetch'
 import router from './router.js'
 
 router.get('/api/bingreports', async function(req, res) {
-  console.log(req.query)
   try {
     const data = await fetch(`http://dev.virtualearth.net/REST/v1/Traffic/Incidents/${req.query.mapArea}/true/?key=${process.env.BING_MAPS_API_KEY}`)
       .then(res => res.json())
-
-    console.log(data)
 
     res.send(data.resourceSets[0].resources.map((resource) => ({
       id: resource.incidentId,
