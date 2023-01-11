945c019017fdac995d7d17420eb4bd4f3310912d
k-lokoy <kennethlokoy@gmail.com>
2023-01-11 12:22:29 +0100

    Converted utility functions to typescript

--- client/src/utils.ts
+++ client/src/utils.ts
@@ -13,13 +13,16 @@ const months = [
   'Des',
 ]
 
-export const dd = (d) => d < 10 ? '0'+d : d
+export const dd = (d: number): string => d < 10 ? '0'+d : d.toString()
 
-export const formatDateTime = (date, showDate) => {
+export const formatDateTime = (date: Date, showDate = false) => {
   const now = new Date()
   const midnight = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0, 0, 0)
-  const dayMonth = showDate || (now.valueOf() - date.valueOf() > now.valueOf() - midnight.valueOf())
-    ?`${date.getDate()} ${months[date.getMonth()]}, ` : ''
+  
+  const dayMonth =
+    showDate || (now.valueOf() - date.valueOf() > now.valueOf() - midnight.valueOf())
+      ? `${date.getDate()} ${months[date.getMonth()]}, `
+      : ''
   
   return `${dayMonth}${dd(date.getHours())}:${dd(date.getMinutes())}`
 }
